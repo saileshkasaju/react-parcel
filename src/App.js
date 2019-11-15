e27e907c9125930f401c1bed479b47a819534e63
Sailesh Kasaju <leosailesh@gmail.com>
2019-11-15 16:48:10 +0545

    feat: add babel plugin for class properties

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import { Router, Link } from "@reach/router";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 
-export const App = () => {
-  return (
-    <div>
-      <header>
-        <nav>
-          <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link>
-        </nav>
-      </header>
-      <Router>
-        <Home path="/" />
-        <Dashboard path="/dashboard" />
-      </Router>
-    </div>
-  );
-};
+class App extends React.Component {
+  // state = { hello: "world" };
+  // clickMe = () => {
+
+  // }
+  render() {
+    return (
+      <div>
+        <header>
+          <nav>
+            <Link to="/">Home {this.state.hello}</Link>{" "}
+            <Link to="dashboard">Dashboard</Link>
+          </nav>
+        </header>
+        <Router>
+          <Home path="/" />
+          <Dashboard path="/dashboard" />
+        </Router>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const TABLE_HEADERS = [];
+export const Colors = {
+  primary: "#3e3e3e",
+  secondary: "#3e3e3e"
+};
